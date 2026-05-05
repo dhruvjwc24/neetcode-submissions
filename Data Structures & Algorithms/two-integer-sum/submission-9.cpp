4ab4c50262561ee69142c93577a7ca608a849cac
Dhruv Chandna <74632827+dhruvjwc24@users.noreply.github.com>
2026-05-04 22:32:09 -0400

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> kv;
+        kv.reserve(nums.size());
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            auto it = kv.find(complement);
+            if (it != kv.end()) {
+                return {it->second, i};
+            }
+            kv[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
