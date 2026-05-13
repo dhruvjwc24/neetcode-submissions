e03ead72a0682758856323ff2530fafeb9a78d28
Dhruv Chandna <74632827+dhruvjwc24@users.noreply.github.com>
2026-05-12 22:50:19 -0400

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> track_row(9);
+        vector<unordered_set<char>> track_col(9);
+        vector<unordered_set<char>> track_block(9);
+
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                char ch = board[r][c];
+                if (ch == '.') { continue; }
+                
+                auto it = track_row[r].find(ch);
+                if (it == track_row[r].end()) {
+                    track_row[r].insert(ch);
+                } else { return false; }
+
+                it = track_col[c].find(ch);
+                if (it == track_col[c].end()) {
+                    track_col[c].insert(ch);
+                } else { return false; }
+
+                it = track_block[r/3 * 3 + c/3].find(ch);
+                if (it == track_block[r/3 * 3 + c/3].end()) {
+                    track_block[r/3 * 3 + c/3].insert(ch);
+                } else { return false; }
+            }
+        }
+
+        return true; 
+    }
+};
