47825da124af99242a05677132b918f393377017
Dhruv Chandna <74632827+dhruvjwc24@users.noreply.github.com>
2026-07-08 18:31:41 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() % 2 != 0) return false;
+        stack<char> stk;
+        set<char> fwdChs = {'(', '{', '['};
+        set<char> bwdChs = {')', '}', ']'};
+        unordered_map<char, char> pam = {{')', '('}, {'}', '{'}, {']', '['}};
+
+        for (int idx = 0; idx < s.size(); ++idx) {
+            // if the char at idx is a fwd char just add to stack
+            if (fwdChs.find(s[idx]) != fwdChs.end()) {
+                stk.emplace(s[idx]);
+            }
+
+            if (bwdChs.find(s[idx]) != bwdChs.end()) {
+                if (stk.empty() || stk.top() != pam[s[idx]]) return false;
+                stk.pop();
+            }
+        }
+
+        if (!stk.empty()) return false;
+
+        return true;
+    }
+};
