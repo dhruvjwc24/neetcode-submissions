9bb4227a64163b7a9030658b2329c42d1d8b6b50
Dhruv Chandna <74632827+dhruvjwc24@users.noreply.github.com>
2026-07-09 10:02:48 -0400

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        vector<int> underlyingContainer;
+        underlyingContainer.reserve(tokens.size());
+        stack<int, vector<int>> stk(std::move(underlyingContainer));
+
+        int tokA, tokB;
+
+        for (string& tok : tokens) {
+            if (tok == "+") {
+                // + sequence
+                tokB = stk.top();
+                stk.pop();
+                tokA = stk.top();
+                stk.pop();
+                stk.push(tokA + tokB);
+            } 
+            
+            else if (tok == "-") {
+                // - sequence
+                tokB = stk.top();
+                stk.pop();
+                tokA = stk.top();
+                stk.pop();
+                stk.push(tokA - tokB);
+            }
+
+            else if (tok == "*") {
+                // * sequence
+                tokB = stk.top();
+                stk.pop();
+                tokA = stk.top();
+                stk.pop();
+                stk.push(tokA * tokB);
+            }
+
+            else if (tok == "/") {
+                // / sequence
+                tokB = stk.top();
+                stk.pop();
+                tokA = stk.top();
+                stk.pop();
+                stk.push(tokA / tokB);
+            }
+
+            else {
+                stk.push(stoi(tok));
+            }
+        }
+
+        return stk.top();
+    }
+};
