bbd01bd631fa5af8a8b99593b2000271fe53a6af
Dhruv Chandna <74632827+dhruvjwc24@users.noreply.github.com>
2026-07-09 11:42:27 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> underlyingContainer;
+        vector<int> result(temperatures.size(), 0);
+        underlyingContainer.reserve(temperatures.size());
+        stack<int, vector<int>> stk(std::move(underlyingContainer));
+
+        for (int idx = 0; idx < temperatures.size(); ++idx) {
+            while (!stk.empty() && temperatures[idx] > temperatures[stk.top()]) {
+                result[stk.top()] = idx - stk.top();
+                stk.pop();
+            }
+
+            stk.push(idx);
+        }
+
+        return result;
+    }
+};
