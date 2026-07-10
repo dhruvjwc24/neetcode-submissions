0d4b872bbe555afc378bf32587cd5f54819cb37b
Dhruv Chandna <74632827+dhruvjwc24@users.noreply.github.com>
2026-07-10 15:07:55 -0400

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.cpp
@@ -0,0 +1,37 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<string, int>>> pam;
+
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        pam[key].emplace_back(value, timestamp);
+    }
+    
+    string get(string key, int timestamp) {
+        auto it = pam.find(key);
+        if (it == pam.end()) return "";
+
+        vector<pair<string, int>>& vals = it->second;
+
+        int lower = 0; int upper = vals.size() - 1;
+        int bestIdx = -1;
+
+        while (lower <= upper) {
+            int mid = lower + (upper - lower) / 2;
+
+            if (vals[mid].second > timestamp) {
+                upper = mid - 1;
+            } else if (vals[mid].second < timestamp) { // need to search right of val
+                bestIdx = mid;
+                lower = mid + 1;
+            } else { // great! we found it, return the string
+                return vals[mid].first; 
+            }
+        }
+
+        return bestIdx == -1 ? "" : vals[bestIdx].first;
+    }
+};
