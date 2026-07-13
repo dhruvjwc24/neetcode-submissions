8c3f22eac240594c65fa85969f8659cf5bd53637
Dhruv Chandna <74632827+dhruvjwc24@users.noreply.github.com>
2026-07-13 12:13:18 -0400

    Add: merge-two-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-3.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy;
+        ListNode* agent = &dummy;
+
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                agent->next = list1;
+                list1 = list1->next;
+            } else {
+                agent->next = list2;
+                list2 = list2->next;
+            }
+
+            agent = agent->next;
+        }
+
+        agent->next = list1 ? list1 : list2;
+        return dummy.next;
+
+        // if (!list1 && !list2) return nullptr;
+        
+        // ListNode* head = new ListNode();
+        // ListNode* agent = head;
+        // ListNode* p1 = list1;
+        // ListNode* p2 = list2;
+        // ListNode* prev = nullptr;
+
+        // while (p1 && p2) {
+        //     if (p1->val < p2->val) {
+        //         agent->val = p1->val;
+        //         p1 = p1->next;
+        //     } else {
+        //         agent->val = p2->val;
+        //         p2 = p2->next;
+        //     }
+            
+        //     prev = agent;
+        //     agent->next = new ListNode();
+        //     agent = agent->next;
+        // }
+
+        // while (p1) {
+        //     agent->val = p1->val;
+        //     p1 = p1->next;
+        //     prev = agent;
+        //     agent->next = new ListNode();
+        //     agent = agent->next;
+        // }
+
+        // while (p2) {
+        //     agent->val = p2->val;
+        //     p2 = p2->next;
+        //     prev = agent;
+        //     agent->next = new ListNode();
+        //     agent = agent->next;
+        // }
+
+        // delete agent;
+        // prev->next = nullptr;
+
+        // return head;
+    }
+};
